6b2847585fa5a4d9ec1d0cd4e17bbe496525842c
Ruben Roberto <ruben.d.roberto@gmail.com>
2022-11-09 18:43:15 +0000

    woocommerce post and get product test

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import dataSource from "@dataSource";
 import express from 'express';
 import Shopify, { ApiVersion, AuthQuery } from '@shopify/shopify-api';
+import { HTTPClientFactory } from '@woocommerce/api';
 require('dotenv').config();
 
-(async () => {
-    await dataSource.initialize()
-    const app = express();
-
+const shopify = (app: express.Application) => {
     const { API_KEY, API_SECRET_KEY, API_VERSION, SCOPES, SHOP, HOST, HOST_SCHEME } = process.env;
 
     console.log(API_KEY)
@@ -69,5 +67,73 @@ require('dotenv').config();
     app.listen(3000, () => {
       console.log('your app is now listening on port 3000');
     });
+
+}
+
+const woocommerce = async (app: express.Application) => {
+
+    const { WOOCOMMERCE_API_KEY, WOOCOMMERCE_API_KEY_SECRET } = process.env;
+    // You can create an API client using the client factory with pre-configured middleware for convenience.
+    //let client = HTTPClientFactory.build('https://localhost:8888')
+    //    .withBasicAuth(WOOCOMMERCE_API_KEY as string, WOOCOMMERCE_API_KEY_SECRET as string)
+    //    .create();
+
+    // You can also create an API client configured for requests using OAuth.
+    let client = HTTPClientFactory.build('http://localhost:8888')
+        .withOAuth(WOOCOMMERCE_API_KEY as string, WOOCOMMERCE_API_KEY_SECRET as string)
+        .create();
+
+    const data = {
+      name: "Premium Quality",
+      type: "simple",
+      regular_price: "21.99",
+      description: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
+      short_description: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+      categories: [
+        {
+          id: 9
+        },
+        {
+          id: 14
+        }
+      ],
+      images: [
+        {
+          src: "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg"
+        },
+        {
+          src: "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg"
+        }
+      ]
+    };
+
+    client.post("/wc/v3/products", data)
+      .then((response: any) => {
+        console.log(response.data);
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+
+    client.get("/wc/v3/products")
+      .then((response) => {
+        console.log(response);
+      }, (error) => console.log(error));
+    //client.get("")
+    //  .then((response) => {
+    //    console.log(response.data);
+    //  })
+    //  .catch((error) => {
+    //    console.log(error);
+    //  });
+}
+
+(async () => {
+    await dataSource.initialize()
+    const app = express();
+
+    // Shopify
+    shopify(app);
+    woocommerce(app);
 })();
 
